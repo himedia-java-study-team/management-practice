aafe9a9d2e91535cf944a4b8d2ab47f0c281a5e2
Eunji <a01082593315@gmail.com>
2024-07-10 19:49:10 +0900

    Feat: edit controller updateBook()

--- management-practice/src/main/java/team/practice/BookController.java
+++ management-practice/src/main/java/team/practice/BookController.java
@@ -24,8 +24,9 @@ public class BookController {
 
     }
 
-    public void updateBook() {
-
+    public void updateBook(int isbn3, String title2) {
+        library.updateBook(isbn3,title2);
+        view.displayMessage(" 도서 수정이 완료 되었습니다! ");
     }
 
     public void deleteBook() {
